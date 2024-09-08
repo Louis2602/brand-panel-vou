0bd06a52630ff815b52856c8326d27c0b5ab8ea0
Tùng Lâm Trần <ttlam.dev@gmail.com>
2024-09-08 21:46:00 +0700

    remove notification

--- src/app/dashboard/_components/header.tsx
+++ src/app/dashboard/_components/header.tsx
@@ -1,14 +1,6 @@
 import { UserButton } from "@/components/auth/user-button";
 import { MobileNavigation } from "./navigation";
 import { useAuth } from "@/providers/auth-provider";
-import { Button } from "@/components/ui/button";
-import { Bell } from "lucide-react";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { NotificationCard } from "./notification-card";
 
 export const Header = () => {
   const { user } = useAuth();
@@ -17,29 +9,6 @@ export const Header = () => {
       <div className="flex items-center gap-2 font-semibold lg:hidden">
         <MobileNavigation />
       </div>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button size="icon" variant="ghost">
-            <div className="relative">
-              <Bell className="h-6 w-6" />
-              <div className="absolute top-0 right-0 -translate-x-1/2 -translate-y-1/2 h-4 w-4 flex items-center justify-center rounded-full bg-rose-500 text-white text-xs font-bold">
-                3
-              </div>
-            </div>
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[350px]" align="end">
-          <div className="grid gap-4">
-            <div className="space-y-2">
-              <h4 className="font-medium leading-none">Notifications</h4>
-              <p className="text-sm text-muted-foreground">
-                You have 3 unread messages.
-              </p>
-            </div>
-            <NotificationCard />
-          </div>
-        </PopoverContent>
-      </Popover>
       <UserButton currentBrand={user} />
     </header>
   );
